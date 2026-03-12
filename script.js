d1f2320bccdd08ccddafa139b8837a303f93248e
shellipal <palshelli5@gmail.com>
2026-03-12 23:27:37 +0530

    project section

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("hello world!!!");
+// console.log("hello world!!!");
+
+const projects = [
+  {
+    id: 1,
+    title: "Todo App",
+    description:
+      "Todo app with add, update and delete functionality with great UI",
+    tech: ["HTML", "CSS", "Javascipt"],
+    github: "https://github.com/shellipal/todo",
+    live: "https://github.com/shellipal/todo",
+  },
+  {
+    id: 2,
+    title: "Blog App",
+    description:
+      "Todo app with add, update and delete functionality with great UI",
+    tech: ["HTML", "CSS", "Javascipt"],
+    github: "https://github.com/shellipal/todo",
+    live: "https://github.com/shellipal/todo",
+  },
+  {
+    id: 3,
+    title: "Portfolio",
+    description:
+      "Todo app with add, update and delete functionality with great UI",
+    tech: ["HTML", "CSS", "Javascipt"],
+    github: "https://github.com/shellipal/todo",
+    live: "https://github.com/shellipal/todo",
+  },
+];
