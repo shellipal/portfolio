237005a90367df76cb8ae039b62749d6591cb95b
shellipal <palshelli5@gmail.com>
2026-03-15 14:54:16 +0530

    dynamic projects

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const projects = [
     title: "Todo App",
     description:
       "Todo app with add, update and delete functionality with great UI",
-    tech: ["HTML", "CSS", "Javascipt"],
+    tech: ["HTML", "CSS", "JS"],
+    image: "/images/projects/bookverse.png",
     github: "https://github.com/shellipal/todo",
     live: "https://github.com/shellipal/todo",
   },
@@ -15,7 +16,8 @@ const projects = [
     title: "Blog App",
     description:
       "Todo app with add, update and delete functionality with great UI",
-    tech: ["HTML", "CSS", "Javascipt"],
+    image: "/images/projects/recipe.png",
+    tech: ["HTML", "CSS", "JS"],
     github: "https://github.com/shellipal/todo",
     live: "https://github.com/shellipal/todo",
   },
@@ -24,8 +26,40 @@ const projects = [
     title: "Portfolio",
     description:
       "Todo app with add, update and delete functionality with great UI",
-    tech: ["HTML", "CSS", "Javascipt"],
+    image: "/images/projects/imggen.png",
+    tech: ["HTML", "CSS", "JS"],
     github: "https://github.com/shellipal/todo",
     live: "https://github.com/shellipal/todo",
   },
 ];
+
+const projectsList = document.getElementById("project-list");
+
+projectsList.innerHTML = projects
+  .map(
+    (project, i) =>
+      `
+    <div class="project-card">
+                <img src=${project.image}
+                    alt=${project.title} />
+
+                <div class="card-info">
+                    <h1 class="card-title">${project.title}</h1>
+                    <p class="card-desc">${project.description}</p>
+                    <p class="card-hash">
+                        ${project.tech.map((t) => `<span>#${t}${" "}</span>`).join("")}
+                    </p>
+                </div>
+                <!-- ------------icons----------------- -->
+                <div class="project-icon">
+                    <span class="icon">
+                        <img src="/images/projects/github.png" alt="github">
+                    </span>
+                    <span class="icon">
+                        <img src="/images/projects/play-button.png" alt="link">
+                    </span>
+                </div>
+            </div>
+  `,
+  )
+  .join("");
